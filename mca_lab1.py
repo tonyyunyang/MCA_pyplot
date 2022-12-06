fc8e80b188b888f662153cdbfadbada64ab4ae1e
Tony <tonyyunyang@outlook.com>
2022-12-07 00:36:20 +0100

    new

--- mca_lab1.py
+++ mca_lab1.py
@@ -7,29 +7,38 @@ import csv
 #         print(line)
 
 x, y = [], []
-for line_area in open(r'C:\Users\tonyy\Desktop\python_plot\mca_lab1_ucbqsort_area.csv'):
+for line_area in open(r'mca_lab1_ucbqsort_area.csv'):
     values = [float(s) for s in line_area.split()]
     x.append(values)
     
-for line_cycle in open(r'C:\Users\tonyy\Desktop\python_plot\mca_lab1_ucbqsort_excycles.csv'):
+for line_cycle in open(r'mca_lab1_ucbqsort_excycles.csv'):
     values = [float(s) for s in line_cycle.split()]
     y.append(values)
     
 a, b = [], []
-for line_area in open(r'C:\Users\tonyy\Desktop\python_plot\mca_lab1_convolution_area.csv'):
+for line_area in open(r'mca_lab1_convolution_area.csv'):
     values = [float(s) for s in line_area.split()]
     a.append(values)
     
-for line_cycle in open(r'C:\Users\tonyy\Desktop\python_plot\mca_lab1_convolution_excycles.csv'):
+for line_cycle in open(r'mca_lab1_convolution_excycles.csv'):
     values = [float(s) for s in line_cycle.split()]
     b.append(values)
     
+plt.title('UCBQSORT', fontsize = 20)
+plt.xlabel('Area', fontsize=15)
+plt.ylabel('Time (Execution Cycles)', fontsize=15)
+plt.scatter(x, y, marker='+', s=50, c='red')
+plt.show()
+
+plt.title('Convolution_5x5', fontsize = 20)
+plt.xlabel('Area', fontsize=15)
+plt.ylabel('Time (Execution Cycles)', fontsize=15)
+plt.scatter(a, b, marker='+', s=50, c='blue')
+plt.show()
     
 plt.title('UCBQSORT VS Convolution_5x5', fontsize = 20)
 plt.xlabel('Area', fontsize=15)
 plt.ylabel('Time (Execution Cycles)', fontsize=15)
-
 plt.scatter(x, y, marker='+', s=50, c='red')
 plt.scatter(a, b, marker='+', s=50, c='blue')
-
 plt.show()
