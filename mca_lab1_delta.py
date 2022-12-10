9974d30369c8d70c89ad4961701981ac4a9e2ef3
Tony <tonyyunyang@outlook.com>
2022-12-10 19:18:47 +0100

    new

--- mca_lab1_delta.py
+++ mca_lab1_delta.py
@@ -21,18 +21,18 @@ for line_cycle in open(r'mca_lab1_delta_convolution_cycle.csv'):
     values = [float(s) for s in line_cycle.split()]
     b.append(values)
     
-# plt.title('UCBQSORT', fontsize = 20)
-# plt.xlabel('Area', fontsize=15)
-# plt.ylabel('Time (Execution Cycles)', fontsize=15)
-# plt.scatter(x, y, marker='+', s=50, c='red')
-# plt.show()
+plt.title('UCBQSORT', fontsize = 20)
+plt.xlabel('Delta Area', fontsize=15)
+plt.ylabel('Addition Time (Execution Cycles)', fontsize=15)
+plt.scatter(x, y, marker='+', s=50, c='red')
+plt.show()
 
-# plt.title('Convolution_5x5', fontsize = 20)
-# plt.xlabel('Area', fontsize=15)
-# plt.ylabel('Time (Execution Cycles)', fontsize=15)
-# plt.scatter(a, b, marker='+', s=50, c='blue')
-# plt.show()
-    
+plt.title('Convolution_5x5', fontsize = 20)
+plt.xlabel('Delta Area', fontsize=15)
+plt.ylabel('Addition Time (Execution Cycles)', fontsize=15)
+plt.scatter(a, b, marker='+', s=50, c='blue')
+plt.show()
+
 plt.title('UCBQSORT VS Convolution_5x5', fontsize = 20)
 plt.xlabel('Delta Area', fontsize=15)
 plt.ylabel('Addition Time (Execution Cycles)', fontsize=15)
