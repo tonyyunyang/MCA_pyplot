b673022f680cc9bba8f693d140cfa92dbaeff2d5
Tony <tonyyunyang@outlook.com>
2022-12-15 11:03:25 +0100

    final

--- mca_lab1.py
+++ mca_lab1.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import math
 
 x, y = [], []
 for line_area in open(r'mca_lab1_ucbqsort_area.csv'):
@@ -21,6 +22,10 @@ for line_cycle in open(r'mca_lab1_convolution_excycles.csv'):
     values = [float(s) for s in line_cycle.split()]
     b.append(values)
     
+    
+x1 = np.linspace(50000, 1200000, 1000000)
+y1 =  4E8 * pow(x1, -0.501)
+    
 plt.title('UCBQSORT', fontsize = 20)
 plt.xlabel('Area', fontsize=15)
 plt.ylabel('Time (Execution Cycles)', fontsize=15)
@@ -31,6 +36,7 @@ plt.title('Convolution_5x5', fontsize = 20)
 plt.xlabel('Area', fontsize=15)
 plt.ylabel('Time (Execution Cycles)', fontsize=15)
 plt.scatter(a, b, marker='+', s=50, c='blue')
+plt.plot(x1, y1)
 plt.show()
     
 plt.title('UCBQSORT VS Convolution_5x5', fontsize = 20)
